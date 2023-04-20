a409eeb50ca12e4fca7c8bde5751974b344a6a51
Anil <anilkrishnakurapati@gmail.com>
2023-04-20 12:57:50 +1000

    child one

---
+++ first/.vscode/conditional.java
@@ -0,0 +1,19 @@
+public class conditional {
+
+    public static void main(String[] args){
+
+        int age = 40;
+
+        if (age>=50)
+
+        {
+            System.out.println("Satisfied");
+        }
+        else
+        {
+            System.out.println("Not Satisfied");                
+        }
+        System.out.println("Done");
+    }
+    
+}
