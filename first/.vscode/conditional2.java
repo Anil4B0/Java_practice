11a99d5c798a201c9f0af5c1fbbd39bd03e48ff6
Anil <anilkrishnakurapati@gmail.com>
2023-04-20 13:34:16 +1000

    condi three example

---
+++ first/.vscode/conditional2.java
@@ -0,0 +1,21 @@
+public class conditional2 {
+    public static void main(String[] args) {
+
+        int a = 3;
+
+        switch (a) {
+
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+
+        }
+
+    }
+}
