f07e83b61c76ebd6c897d928af186ca96db288fc
Anil <anilkrishnakurapati@gmail.com>
2023-04-24 21:36:14 +1000

    loops

--- first/Practice/loops.java
+++ first/Practice/loops.java
@@ -2,11 +2,11 @@ public class loops {
 
     public static void main(String[] args) {
 
-        int number = 1;
+        int number = 2;
 
         while (number <= 10) {
             System.out.println("number is = " + number);
-            number++;
+            number += 2;
 
         }
 
