9fbf6a53cdf8a519989d8d199321a5cad7f7e836
Anil <anilkrishnakurapati@gmail.com>
2023-04-25 13:05:40 +1000

    changes

--- first/Practice/assignmenst.java
+++ first/Practice/assignmenst.java
@@ -4,6 +4,7 @@ public class assignmenst {
 
         int a = 10;
         int b = 20;
+        int d = 50;
 
         int c;
 
@@ -20,6 +21,15 @@ public class assignmenst {
 
             System.out.println("odd number");
         }
+
+        if ((a > b) && (a > d)) {
+            System.out.println("a is bigger");
+        } else if ((b > a) && (b > c)) {
+
+            System.out.println("b is bigger");
+        }
+
+        System.out.println("c is bigger");
 
     }
 
