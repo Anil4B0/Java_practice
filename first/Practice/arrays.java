55a5687d18016cf8a6d70e5a01ca9805b5c2e431
Anil <anilkrishnakurapati@gmail.com>
2023-04-26 22:27:03 +1000

    arrays

---
+++ first/Practice/arrays.java
@@ -0,0 +1,20 @@
+public class arrays {
+
+    public static void main(String[] args) {
+
+        String a[] = new String[3];
+
+        a[0] = "Anil";
+        a[1] = "krishna";
+        a[2] = "Raju";
+
+        System.out.println(a.length);
+
+        for (String i : a) {
+
+            System.out.println(i);
+        }
+
+    }
+
+}
