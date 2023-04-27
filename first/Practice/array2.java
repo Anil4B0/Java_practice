5eba62b24a2768f954450c0551d0b6946f8d5e49
Anil <anilkrishnakurapati@gmail.com>
2023-04-27 21:39:07 +1000

    new arrays

--- first/Practice/array2.java
+++ first/Practice/array2.java
@@ -2,14 +2,19 @@ public class array2 {
     public static void main(String[] args) {
 
         int val[] = { 5, 10, 20, 30 };
+        Object a[] = { "Anil", 10, 12.3, 'K' };
 
         int sum = 0;
 
         for (int i : val) {
             sum += i;
         }
-
         System.out.println("addition of all = " + sum);
+
+        for (Object i : a) {
+
+            System.out.println("values = " + i);
+        }
 
     }
 }
