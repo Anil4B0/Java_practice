7fdffb887911bebacdc4f486cf9e01f660aefd0b
Anil <anilkrishnakurapati@gmail.com>
2023-05-02 20:35:04 +1000

    2D arrays

---
+++ first/Practice/arrays3.java
@@ -0,0 +1,26 @@
+public class arrays3 {
+
+    public static void main(String[] args) {
+
+        int a[][] = new int[3][2];
+
+        a[0][0] = 10;
+        a[0][1] = 20;
+        a[1][0] = 30;
+        a[1][1] = 40;
+        a[2][0] = 50;
+        a[2][1] = 60;
+
+        for (int i[] : a) {
+            for (int j : i) {
+
+                System.out.print("print " + j + " ");
+            }
+
+            System.out.println("  ");
+
+        }
+
+    }
+
+}
