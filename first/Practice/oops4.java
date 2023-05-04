43142fa66ddfd853dfc9b38278d01b7f34c49a41
Anil <anilkrishnakurapati@gmail.com>
2023-05-04 21:21:45 +1000

    oops

---
+++ first/Practice/oops4.java
@@ -0,0 +1,21 @@
+public class oops4 {
+
+    int a;
+    int b;
+
+    public static void main(String[] args) {
+
+        oops4 add = new oops4();
+
+        add.a = 100;
+        add.b = 100;
+        add.sum();
+
+    }
+
+    void sum() {
+        System.out.println(a + b);
+
+    }
+
+}
