dec36115b8d5fb173e29df1d17678067ec8a5e05
Anil4B0 <113575480+Anil4B0@users.noreply.github.com>
2023-05-11 05:28:51 +0000

    override

---
+++ first/Practice/override.java
@@ -0,0 +1,27 @@
+public class override {
+    
+     public static void main(String[] args){
+
+        vehical obj = new vehical();
+
+        System.out.println(obj.vehi1());
+
+
+     }
+}
+
+class vehical{
+
+    int  vehi1() {
+
+        return 14;
+    }
+}
+
+class bike{
+
+    int vehi1(){
+
+        return 2;
+    }
+}
